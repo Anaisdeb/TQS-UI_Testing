311bcb26c734b9582aa3d0f58bca0d965915c0af
Anais Debureaux <anaisdeb00@gmail.com>
2022-12-10 16:13:02 +0100

    feature 1 completed: 6 scenarios

--- src/test/java/features/login.feature
+++ src/test/java/features/login.feature
@@ -4,16 +4,16 @@ Scenario: Login with credentials
 
 Given the user is on the application landing page
 When the user clicks the account option
-And the user clicks the <profile> option
+And the user clicks the <profile> login
 And the user enters valid <email> and valid <password>
 And the user clicks the login button
-Then the user login is <status>
+Then the user login is <login>
 
 Examples:
-|profile|email|password|status|
-|Customer|user@phptravels.com|demouser|valid|
-|Agents|agent@phptravels.com|demoagent|valid|
-|Supplier|supplier@phptravels.com|demosupplier|valid|
-|Customer|user@phptravels.com|a|invalid|
-|Agents|agent@phptravels.com|b|invalid|
-|Supplier|supplier@phptravels.com|c|invalid|
+|profile|email|password|login|
+|Customer|user@phptravels.com|demouser|1|
+|Agents|agent@phptravels.com|demoagent|1|
+|Supplier|supplier@phptravels.com|demosupplier|1|
+|Customer|user@phptravels.com|a|0|
+|Agents|agent@phptravels.com|b|0|
+|Supplier|supplier@phptravels.com|c|0|
--- src/test/java/steps/LoginSteps.java
+++ src/test/java/steps/LoginSteps.java
@@ -1,8 +1,17 @@
 package steps;
 
+import java.util.Optional;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+import io.cucumber.java.After;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 
 public class LoginSteps {
 	
@@ -15,4 +24,49 @@ public class LoginSteps {
 		driver = new ChromeDriver();
 		driver.navigate().to("https://phptravels.net");
 	}
+	
+	@When("the user clicks the account option")
+	public void theUserClicksTheAccountOption()
+	{
+		driver.findElement(By.id("ACCOUNT")).click();
+	}
+	
+	@When("^the user clicks the (.*) login")
+	public void theUserClicksTheLogin(String profile)
+	{
+		driver.findElement(By.linkText(profile + " Login")).click();
+		
+		// Switch to new window opened
+		for(String winHandle : driver.getWindowHandles()){
+		    driver.switchTo().window(winHandle);
+		}
+	}
+	
+	@When("^the user enters valid (.*) and valid (.*)")
+	public void theUserEntersValidAndValid(String email, String password)
+	{
+		driver.findElement(By.cssSelector("input[name='email']")).sendKeys(email);
+		driver.findElement(By.cssSelector("input[name='password']")).sendKeys(password);
+	}
+	
+	@When("^the user clicks the login button")
+	public void theUserClicksTheLoginButton()
+	{
+		driver.findElement(By.cssSelector("button[type='submit']")).click();
+	}
+	
+	@Then("^the user login is (.*)")
+	public void theUserLoginIs(Boolean login)
+	{
+
+		String content = driver.findElement(By.tagName("body")).getText();
+		Assert.assertEquals(login, content.contains("Dashboard"));
+		
+	}
+	
+	 @After
+     public void closeTheBrowser() {
+         driver.close();
+         driver.quit();
+     }
 }
